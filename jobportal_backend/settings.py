df077ab2a76ef701d4397f6a46d55606fea0cfa8
Divakar Singh <ds299555@gmail.com>
2025-02-16 19:02:38 +0530

    update

--- jobportal_backend/settings.py
+++ jobportal_backend/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = 'django-insecure-n*5gt$m6(cpb3z1^nzfr66futc33m1bq!#jd#zo(%e527*@x!i
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['.vercel.app',"localhost","127.0.0.1"]
+ALLOWED_HOSTS = ['.onrender.com',"localhost","127.0.0.1"]
 
 
 # Application definition
